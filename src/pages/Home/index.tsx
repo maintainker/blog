a7672cd5eeffaa8cebfcf740b9cbb76ebb0b36af
maintainker <syhwan88@naver.com>
2021-11-14 15:29:18 +0900

    [fix] error

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import fs from "fs";
 
-fs.readdir("/post", (err, fileList) => {
-  console.log(fileList);
-});
 const Home: React.FC = () => {
   return (
     <>
