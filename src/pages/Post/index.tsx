2900b76e216dfbabc0eb322d8d749ada2988d420
maintainker <syhwan88@naver.com>
2021-11-20 20:07:50 +0900

    [feat] end project

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import ReactMarkdown from "react-markdown";
-import axios from "axios";
-import remarkGfm from "remark-gfm";
-import rehypeKatex from "rehype-katex";
-import { blogList } from "../../constants/list";
-import { useLocation, useParams } from "react-router";
-import * as MDStyle from "../../components/posts/markdown";
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+import axios from 'axios';
+import remarkGfm from 'remark-gfm';
+import rehypeKatex from 'rehype-katex';
+import { blogList } from '../../constants/list';
+import { useLocation, useParams } from 'react-router';
+import * as MDStyle from '../../components/posts/markdown';
 
 const PostView: React.FC = () => {
   const location = useLocation();
@@ -18,9 +18,8 @@ const PostView: React.FC = () => {
       imageUrl: string;
     } = {
       isGet: false,
-      post: "",
-      imageUrl:
-        "https://raw.githubusercontent.com/maintainker/blog/main/public/image/post-bg-desk.jpeg",
+      post: '',
+      imageUrl: 'https://raw.githubusercontent.com/maintainker/blog/main/public/image/post-bg-desk.jpeg',
     };
     if (location.state?.post) {
       thisState.isGet = true;
@@ -38,9 +37,7 @@ const PostView: React.FC = () => {
       try {
         if (!data.isGet) {
           const response = await axios.get(
-            `https://raw.githubusercontent.com/maintainker/blog/main/public/post/${
-              blogList[Number(id)].file
-            }`
+            `https://raw.githubusercontent.com/maintainker/blog/main/public/post/${blogList[Number(id)].file}`,
           );
           setData({ ...data, isGet: true, post: response.data });
         }
@@ -52,20 +49,21 @@ const PostView: React.FC = () => {
   }, [data.isGet, id]);
   return (
     <Content>
-      <Thumbnail>
-        <img src={data.imageUrl} alt="썸네일 이미지" />
+      <Thumbnail imageUrl={data.imageUrl}>
+        <InfoBox>
+          <h1>{blogList[Number(id)].title}</h1>
+          <p>{blogList[Number(id)].subtitle}</p>
+        </InfoBox>
       </Thumbnail>
       <div className="container">
         <ReactMarkdown
           remarkPlugins={[remarkGfm]}
           rehypePlugins={[rehypeKatex]}
           components={{
-            h1: "h2",
+            h1: 'h2',
             code: MDStyle.Code,
-            ul: ({ ...props }) => (
-              <ul style={{ listStyle: "inside" }} {...props} />
-            ),
-            em: ({ ...props }) => <i style={{ color: "red" }} {...props} />,
+            ul: ({ ...props }) => <ul style={{ listStyle: 'inside' }} {...props} />,
+            em: ({ ...props }) => <i style={{ color: 'red' }} {...props} />,
           }}
         >
           {data.post}
@@ -76,20 +74,30 @@ const PostView: React.FC = () => {
 };
 
 export default PostView;
-const Thumbnail = styled.section`
+const Thumbnail = styled.section<{ imageUrl: string }>`
   width: 100%;
   height: 500px;
   overflow: hidden;
   position: relative;
-  img {
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
+  background-image: url(${(p) => p.imageUrl});
+  background-position: center;
+  background-size: cover;
+`;
+const InfoBox = styled.section`
+  min-width: 350px;
+  width: 50%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: rgba(0, 0, 0, 0.4);
+  padding: 20px;
+  color: #fff;
+  h1 {
+    margin-top: 0;
+    font-size: 2em;
   }
 `;
-
 const Content = styled.section`
   width: 100%;
   padding: 0 0 60px;
